4ef199c8fdae65e6e9c3392d17a4eb3c9c289b57
Earthson Lu <Earthson.Lu@gmail.com>
2016-10-30 00:51:07 +0800

    update

--- sync_tushare/incr.py
+++ sync_tushare/incr.py
@@ -3,7 +3,7 @@ import asyncio
 import tushare as ts
 from db import tushare_db
 from task.executor import TaskExecutor
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time as dtime
 
 date_fmt = '%Y-%m-%d'
 
@@ -18,21 +18,36 @@ def incr_run():
     stocks = [v for v in list(tushare_db.execute("""SELECT "code", "timeToMarket" from stock_basics where "timeToMarket" > 0 """))]
     loop = asyncio.get_event_loop()
     async def incr_stock(stock_code, start_date, texecutor):
+        logging.debug("stock: '%s', '%s'" % (stock_code, start_date))
         last_tick = await texecutor.group_last("tick_%s" % stock_code)
         last_history_faa = await texecutor.group_last("history_faa_%s" % stock_code)
         current_time = datetime.now()
         last_tick_schedule_at = last_tick["scheduled_at"] if last_tick is not None else None
         last_history_faa_schedule_at = last_history_faa["scheduled_at"] if last_history_faa is not None else None
         t_delta = timedelta(days=1, hours=1)
-        d_delta = timedelta(days=1)
-        while last_tick_schedule_at is None or last_tick_schedule_at.date() < (current_time + t_delta).date():
-            target_date = last_tick_schedule_at.date()
-            last_tick_schedule_at = target_date + t_delta
-            await texecutor.task_schedule('tick', '%s_%s' % (stock_code, target_date.strftime(date_fmt)), last_tick_schedule_at, group='tick_%s' % stock_code)
-        while last_history_faa_schedule_at is None or last_history_faa_schedule_at.date() < (current_time + t_delta).date():
-            target_date = last_history_faa_schedule_at.date()
-            last_history_faa_schedule_at = target_date + t_delta
-            await texecutor.task_schedule('history_faa', '%s_%s' % (stock_code, target_date.strftime(date_fmt)), last_history_faa_schedule_at)
+        async def add_tick_task(target_date, scheduled_at):
+            options = {
+                "kwargs": {
+                    "stock": stock_code,
+                    "date": target_date.strftime(date_fmt)
+                }
+            }
+            return await texecutor.task_schedule('tick', '%s_%s' % (stock_code, target_date.strftime(date_fmt)), scheduled_at, group='tick_%s' % stock_code, options=options)
+        async def add_history_faa_task(target_date, scheduled_at):
+            options = {
+                "kwargs": {
+                    "stock": stock_code,
+                    "start": target_date.strftime(date_fmt),
+                    "end": target_date.strftime(date_fmt)
+                }
+            }
+            return await texecutor.task_schedule('history_faa', '%s_%s' % (stock_code, target_date.strftime(date_fmt)), scheduled_at, group='history_faa_%s' % stock_code, options=options)
+        async def with_date_update(last_datetime, act):
+            while last_datetime is None or last_datetime.date() < (current_time + t_delta).date():
+                target_date = datetime.combine(last_datetime.date(), dtime.min) if last_datetime is not None else start_date
+                last_datetime = target_date + t_delta
+                await act(target_date, last_datetime)
+        await asyncio.gather(*(with_date_update(last_tick_schedule_at, add_tick_task), with_date_update(last_history_faa_schedule_at, add_history_faa_task)))
 
     with TaskExecutor.load("conf/config.yaml", loop=loop) as tx:
         loop.run_until_complete(asyncio.gather(*(incr_stock(code, datetime.strptime(str(start_date), '%Y%m%d'), tx) for code, start_date in stocks)))                       
--- sync_tushare/worker.py
+++ sync_tushare/worker.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 import tushare as ts
 from db import tushare_db
@@ -10,17 +11,20 @@ tx = TaskExecutor.load("conf/config.yaml", loop=loop)
 def fetch_tick(stock, date):
     df = ts.get_tick_data(stock, date=date)
     if df is None:
+        logging.debug("no tick data for stock: ts.get_tick_data('%s', date='%s')" % (stock, date))
+        return
+    if len(df) > 0 and "当天没有数据" in df['time'][0]:
+        # no data found
+        logging.debug("no tick data for stock: ts.get_tick_data('%s', date='%s')" % (stock, date))
         return
     df['stock'] = stock
     df['date'] = date
     ans = df.set_index(['stock', 'date'])
-    if len(ans) > 0 and "当天没有数据" in ans['time'][0]:
-        # no data found
-        return
     try:
         tushare_db.execute("""delete from tick_data where "stock"='%s' AND "date"='%s' """ % (stock, date))
     except:
         pass
+    logging.debug("data got: ts.get_tick_data('%s', date='%s')" % (stock, date))
     ans.to_sql('tick_data', tushare_db, if_exists="append")
 
 
@@ -31,14 +35,17 @@ def fetch_history_faa(stock, start, end):
     """
     df = ts.get_h_data(stock, autype='hfq', start=start, end=end)
     if df is None:
+        logging.debug("no history data for stock: ts.get_h_data('%s', autype='hfq', start='%s', end='%s')" % (stock, start, end))
         return
     df['stock'] = stock
-    ans = df.set_index(['stock','date'])
+    ans = df.reset_index().set_index(['stock','date'])
     try:
-        tushare_db.execute("""delete from history_faa where "stock"='%s' AND "date">='%s' AND "date"<='%s'""" % (stock, start, end))
+        logging.debug("no history data for stock: ts.get_h_data('%s', autype='hfq', start='%s', end='%s')" % (stock, start, end))
     except:
         pass
+    logging.debug("data got: ts.get_h_data('%s', autype='hfq', start='%s', end='%s')" % (stock, start, end))
     ans.to_sql('history_faa', tushare_db, if_exists="append")
 
+logging.basicConfig(level=logging.DEBUG)
 loop.run_until_complete(tx.run())
 tx.close()
