fe227c1e606f03c53f4f2f62761eed9c053f2106
Earthson Lu <Earthson.Lu@gmail.com>
2016-11-03 15:09:21 +0800

    add time log

--- sync_tushare/worker.py
+++ sync_tushare/worker.py
@@ -5,6 +5,7 @@ import tushare as ts
 from sqlalchemy import TIMESTAMP
 from common import tushare_db
 from task.executor import TaskExecutor
+from task.timeutil import *
 from datetime import datetime
 import time
 
@@ -13,9 +14,13 @@ tx = TaskExecutor.load("conf/config.yaml", loop=loop, multi_process=True)
 
 time_fmt = '%Y-%m-%d %H:%M:%S'
 
+def logtime(key):
+    return lambda t: logging.debug("%s: %.3f", key, t)
+
 @tx.register("tick", expand_param=True)
 def fetch_tick(stock, date):
-    df = ts.get_tick_data(stock, date=date)
+    with timer(logtime("get_tick_data")):
+        df = ts.get_tick_data(stock, date=date)
     if df is None:
         logging.debug("no tick data for stock: ts.get_tick_data('%s', date='%s')" % (stock, date))
         return
@@ -23,16 +28,19 @@ def fetch_tick(stock, date):
         # no data found
         logging.debug("no tick data for stock: ts.get_tick_data('%s', date='%s')" % (stock, date))
         return
-    df['stock'] = stock
-    df['time'] = (date + ' ' + df['time']).map(lambda x: pd.Timestamp(datetime.strptime(x, time_fmt), tz='Asia/Shanghai').strftime(format="%Y-%m-%d %H:%M:%S%z"))
-    ans = df.set_index(['stock', 'time'])
+    with timer(logtime("tick_data_proc")):
+        df['stock'] = stock
+        df['time'] = (date + ' ' + df['time']).map(lambda x: pd.Timestamp(datetime.strptime(x, time_fmt), tz='Asia/Shanghai').strftime(format="%Y-%m-%d %H:%M:%S%z"))
+        ans = df.set_index(['stock', 'time'])
     with tushare_db.connect() as conn:
         try:
-            conn.execute("""delete from tick_data where "stock"='%s' AND "time"::date = date '%s' """ % (stock, date))
+            with timer(logtime("tick_data_del")):
+                conn.execute("""delete from tick_data where "stock"='%s' AND "time"::date = date '%s' """ % (stock, date))
         except:
             pass
         logging.debug("data got: ts.get_tick_data('%s', date='%s')" % (stock, date))
-        ans.to_sql('tick_data', conn, if_exists="append", dtype={'time': TIMESTAMP(timezone=True)})
+        with timer(logtime("tick_data_append")):
+            ans.to_sql('tick_data', conn, if_exists="append", dtype={'time': TIMESTAMP(timezone=True)})
 
 
 @tx.register("history_faa", expand_param=True)
@@ -40,19 +48,23 @@ def fetch_history_faa(stock, start, end):
     """
     History data forward answer authority
     """
-    df = ts.get_h_data(stock, autype='hfq', start=start, end=end)
+    with timer(logtime("get_history_faa")):
+        df = ts.get_h_data(stock, autype='hfq', start=start, end=end)
     if df is None:
         logging.debug("no history data for stock: ts.get_h_data('%s', autype='hfq', start='%s', end='%s')" % (stock, start, end))
         return
-    df['stock'] = stock
-    ans = df.reset_index().set_index(['stock','date'])
+    with timer(logtime("history_faa_proc")):
+        df['stock'] = stock
+        ans = df.reset_index().set_index(['stock','date'])
     with tushare_db.connect() as conn:
         try:
-            conn.execute("""delete from history_faa where "stock"='%s' AND "date">='%s' AND "date"<='%s'""" % (stock, start, end))
+            with timer(logtime("history_faa_del")):
+                conn.execute("""delete from history_faa where "stock"='%s' AND "date">='%s' AND "date"<='%s'""" % (stock, start, end))
         except:
             pass
         logging.debug("data got: ts.get_h_data('%s', autype='hfq', start='%s', end='%s')" % (stock, start, end))
-        ans.to_sql('history_faa', conn, if_exists="append")
+        with timer(logtime("history_faa_append")):
+            ans.to_sql('history_faa', conn, if_exists="append")
 
 logging.basicConfig(level=logging.DEBUG)
 loop.run_until_complete(tx.run())
