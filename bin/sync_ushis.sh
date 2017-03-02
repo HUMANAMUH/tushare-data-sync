993281c38e0d62d539b971250032805cf505563f
Earthson Lu <Earthson.Lu@gmail.com>
2017-03-02 13:53:28 +0800

    remove ushis

--- bin/sync_ushis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PY=$1
-SYNC_DATE=$2
-CMD="$PY /home/mercy/git/US-stock-data/sync.py $SYNC_DATE"
-$PY -m QPhantom.exec.guard --to luyunfei@ppmoney.com --project USHIS_DATA --label Daily_sync $CMD
